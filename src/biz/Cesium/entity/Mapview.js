61f74e722afaee9dedb738ff2f1459bdfa93b6d1
abc-0886kAX-code <120835827@qq.com>
2023-12-05 17:21:41 +0800

    底图更换天地图、初始化增加中文标注

--- src/biz/Cesium/entity/Mapview.js
+++ src/biz/Cesium/entity/Mapview.js
@@ -2,11 +2,11 @@
  * @Author: zhangxin
  * @Date: 2022-04-25 09:19:13
  * @LastEditors: zhangxin
- * @LastEditTime: 2023-12-05 14:22:06
+ * @LastEditTime: 2023-12-05 17:16:58
  * @Description: file content
  */
 import { uuid } from "@/shared/uuid.js";
-import { Viewer, EventHelper, Ion, ScreenSpaceEventHandler, defined, ScreenSpaceEventType } from 'cesium'
+import { Viewer, EventHelper, Ion, WebMapTileServiceImageryProvider, GeographicTilingScheme } from 'cesium'
 const { VITE_CESIUMTOKEN } = import.meta.env;
 
 export class Mapview {
@@ -34,6 +34,23 @@ export class Mapview {
         this.view = new Viewer(mapbox, this.config);
         this.view._cesiumWidget._creditContainer.style.display = 'none'; // 隐藏Cesium logo
         this.view.scene.debugShowFramesPerSecond = true; // 显示帧率
+
+        // 天地图标注
+        this.view.imageryLayers.addImageryProvider(new WebMapTileServiceImageryProvider({
+            url: "http://{s}.tianditu.gov.cn/cia_c/wmts?service=wmts&request=GetTile&version=1.0.0" +
+                "&LAYER=cia&tileMatrixSet=c&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}" +
+                "&style=default&format=tiles&tk=" + 'ef590000990e247813bf916bdce1d941',
+            layer: "tdtCiaLayer",
+            style: "default",
+            format: "tiles",
+            tileMatrixSetID: "c",
+            subdomains: ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7"],
+            tilingScheme: new GeographicTilingScheme(),
+            tileMatrixLabels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"],
+            maximumLevel: 50,
+            show: true
+        }))
+
         return this.view;
     }
     uninstall() {
--- src/config/cesium.conf.js
+++ src/config/cesium.conf.js
@@ -1,9 +1,9 @@
 /*
- * @FilePath: \vue3x_cesium_template\src\config\cesiumconfig.js
+ * @FilePath: \vue3x_cesium_template\src\config\cesium.conf.js
  * @Author: zhangxin
  * @Date: 2023-11-29 14:20:16
  * @LastEditors: zhangxin
- * @LastEditTime: 2023-12-01 17:07:25
+ * @LastEditTime: 2023-12-05 17:11:21
  * @Description:
  */
 import * as Cesium from 'cesium';
@@ -13,7 +13,7 @@ const config = {
     infoBox: false, // 创建InfoBox小部件 默认true - false解决报错
     timeline: false, // 创建时间轴小部件 默认true
     animation: false, // 创建动画小部件 默认true
-    baseLayerPicker: true,//地图图层组件
+    baseLayerPicker: false,//地图图层组件
     fullscreenButton: true,//全屏组件
     geocoder: true,//地理编码搜索组件
     homeButton: true,//首页组件
@@ -23,6 +23,22 @@ const config = {
     navigationInstructionsInitiallyVisible: false, // 如果导航说明最初应该是可见的，则为true；如果直到用户明确单击该按钮，则不显示该说明，否则为false。
     terrain: Cesium.Terrain.fromWorldTerrain(),
     selectionIndicator: false, // 选中图层后 显示的默认绿色框
+    // 天地图底图
+    baseLayer: new Cesium.ImageryLayer(new Cesium.WebMapTileServiceImageryProvider({
+        //影像底图
+        url: "http://{s}.tianditu.gov.cn/img_c/wmts?service=wmts&request=GetTile&version=1.0.0" +
+            "&LAYER=img&tileMatrixSet=c&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}" +
+            "&style=default&format=tiles&tk=" + 'ef590000990e247813bf916bdce1d941',
+        layer: "tdtImgLayer",
+        style: "default",
+        format: "tiles",
+        tileMatrixSetID: "c",
+        subdomains: ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7"],
+        tilingScheme: new Cesium.GeographicTilingScheme(),
+        tileMatrixLabels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"],
+        maximumLevel: 50,
+        show: true
+    })),
 }
 const layers = [
     {
