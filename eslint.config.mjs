f26437de7062dce9a0317583e871c1e86b0638b0
abc-0886kAX-code <120835827@qq.com>
2024-07-25 16:47:24 +0800

    :heavy_plus_sign:
    :heavy_plus_sign:

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+/*
+ * @FilePath: \vue3x_cesium_template\eslint.config.mjs
+ * @Author: abc-0886kAX-code
+ * @Date: 2024-07-25 16:35:19
+ * @LastEditors: abc-0886kAX-code
+ * @LastEditTime: 2024-07-25 16:35:28
+ * @Description:
+ */
+// eslint.config.mjs
+import antfu from '@antfu/eslint-config'
+
+export default antfu()
--- src/biz/Cesium/view/cesium-container/cesium-container.vue
+++ src/biz/Cesium/view/cesium-container/cesium-container.vue
@@ -3,59 +3,57 @@
  * @Author: abc-0886kAX-code
  * @Date: 2023-11-29 10:45:41
  * @LastEditors: abc-0886kAX-code
- * @LastEditTime: 2023-12-05 15:43:15
+ * @LastEditTime: 2024-07-25 16:37:01
  * @Description:
 -->
 <script setup>
-import FloatWindowView from "@/components/float-window/float-window.vue";
+import FloatWindowView from '@/components/float-window/float-window.vue'
 
-import { CesiumFloatSymbolName } from '@/biz/Cesium/share/context';
-import { useCesiumSetup } from "@/biz/Cesium/usecase/useCesiumSetup";
-import { useLayerSetup } from "@/biz/Cesium/usecase/useLayerSetup";
-import { useFloatWindow } from "@/hooks/useFloatWindow";
-
+import { CesiumFloatSymbolName } from '@/biz/Cesium/share/context'
+import { useCesiumSetup } from '@/biz/Cesium/usecase/useCesiumSetup'
+import { useLayerSetup } from '@/biz/Cesium/usecase/useLayerSetup'
+import { useFloatWindow } from '@/hooks/useFloatWindow'
 
 const props = defineProps({
-    config: {
-        type: Object,
-        default: () => ({}),
-    },
-    layers: {
-        type: Array,
-        default: () => [],
-    },
-});
-const emits = defineEmits(["onReady"]);
+  config: {
+    type: Object,
+    default: () => ({}),
+  },
+  layers: {
+    type: Array,
+    default: () => [],
+  },
+})
+const emits = defineEmits(['onReady'])
 
-const { isMounted, loading, loadStyle, mapbox, mapview } = useCesiumSetup(props, emits);
-useLayerSetup(props, mapview);
+const { isMounted, loading, loadStyle, mapbox, mapview } = useCesiumSetup(props, emits)
+useLayerSetup(props, mapview)
 const {
-    floatWindowVisible,
-    floatWindowBind,
-    setContent,
-    setupVisable,
-    setupFloatShow,
-    setupFloatHide,
-    setupFloatWindow,
-} = useFloatWindow();
+  floatWindowVisible,
+  floatWindowBind,
+  setContent,
+  setupVisable,
+  setupFloatShow,
+  setupFloatHide,
+  setupFloatWindow,
+} = useFloatWindow()
 
 provide(CesiumFloatSymbolName, {
-    floatWindowVisible,
-    floatWindowBind,
-    setContent,
-    setupVisable,
-    setupFloatShow,
-    setupFloatHide,
-    setupFloatWindow,
-});
-
+  floatWindowVisible,
+  floatWindowBind,
+  setContent,
+  setupVisable,
+  setupFloatShow,
+  setupFloatHide,
+  setupFloatWindow,
+})
 </script>
 
 <template>
-    <div class="cesium-container" ref="mapbox" v-loading="loading" v-bind="loadStyle">
-        <FloatWindowView :visable.sync="floatWindowVisible" v-bind="floatWindowBind"> </FloatWindowView>
-        <slot v-if="isMounted"> </slot>
-    </div>
+  <div ref="mapbox" v-loading="loading" class="cesium-container" v-bind="loadStyle">
+    <FloatWindowView v-model:visable="floatWindowVisible" v-bind="floatWindowBind" />
+    <slot v-if="isMounted" />
+  </div>
 </template>
 
 <style lang="scss" scoped>
